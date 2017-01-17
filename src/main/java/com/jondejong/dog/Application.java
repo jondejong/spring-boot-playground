7d7ea0c546abfdef55da39c1e8d2f668c8657da4
Jon DeJong <jon.dejong@gmail.com>
2017-01-16 21:40:03 -0600

    just backing up

---
+++ src/main/java/com/jondejong/dog/Application.java
@@ -0,0 +1,17 @@
+package com.jondejong.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jondejong on 1/16/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/jondejong/dog/DogController.java
@@ -0,0 +1,16 @@
+package com.jondejong.dog;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jondejong on 1/16/17.
+ */
+@RestController
+public class DogController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
